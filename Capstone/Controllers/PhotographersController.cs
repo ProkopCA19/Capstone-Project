f6483d43c0d183a8ce06a66b618331f66aeed144
ProkopCA19 <ProkopCA19@gmail.com>
2018-08-14 10:13:15 -0500

    created functionality to search by price range & zipcode

--- Capstone/Controllers/PhotographersController.cs
+++ Capstone/Controllers/PhotographersController.cs
@@ -22,33 +22,33 @@ namespace Capstone.Controllers
 
 
 
-        public ActionResult FilteredPhotographers(bool searchBy, string searchString)
+        public ActionResult FilteredPhotographers(string searchString, bool? searchBy, bool? searchBy2, bool? searchBy3, bool? searchBy4)
         {
-            Client client = new Client();
+         
 
             var photographerList = from s in db.Photographers
                                select s;
-
-            string currentUserId = User.Identity.GetUserId();
-            var thisClient = db.Clients.Include(c=>c.Appointment).Include(q=>q.User).Where(c =>c.UserId == currentUserId).FirstOrDefault();
-            int theZipCode = thisClient.Zipcode;
-            //photographerList = photographerList.Where(s => s.Zipcode == theZipCode);
+         
 
             if (searchBy == true)
             {
-                return View(db.Photographers.Where(p => p.PriceRange1 == searchBy).ToList());
+                photographerList = db.Photographers.Where(p => p.PriceRange1 == searchBy);
+                
             }
-            else if (searchBy == true)
+            else if (searchBy2 == true )
             {
-                return View(db.Photographers.Where(p => p.PriceRange2 == searchBy).ToList());
+                photographerList = db.Photographers.Where(p => p.PriceRange2 == searchBy2);
+               
             }
-            else if (searchBy == true)
+            else if (searchBy3 == true)
             {
-                return View(db.Photographers.Where(p => p.PriceRange3 == searchBy).ToList());
+                photographerList = db.Photographers.Where(p => p.PriceRange3 == searchBy3);
+               
             }
-            else if (searchBy == true)
+            else if (searchBy4 == true)
             {
-                return View(db.Photographers.Where(p => p.PriceRange4 == searchBy).ToList());
+                photographerList = db.Photographers.Where(p => p.PriceRange4 == searchBy4);
+                
             }
 
             if (!String.IsNullOrEmpty(searchString))
@@ -60,35 +60,6 @@ namespace Capstone.Controllers
             return View(photographerList.ToList());
         }
     
-
-    //public ActionResult FilterPhotographers()
-    //{
-    //    var userId = User.Identity.GetUserId();
-    //    var client = db.Clients.Where(c => c.UserId == userId).FirstOrDefault();
-
-    //    if(client.PriceRange1 == true)
-    //    {
-    //        var photographerList = db.Photographers.Where(p => p.PriceRange1 == true);
-    //        return View(photographerList);
-    //    }
-    //    else if(client.PriceRange2 == true)
-    //    {
-    //        var photographerList = db.Photographers.Where(p => p.PriceRange2 == true);
-    //        return View(photographerList);
-    //    }
-    //    else if (client.PriceRange4 == true)
-    //    {
-    //        var photographerList = db.Photographers.Where(p => p.PriceRange4 == true);
-    //        return View(photographerList);
-    //    }
-    //    else
-    //    {
-    //        return View();
-    //    }
-
-
-
-    //}
 
 
     public ActionResult Details(int? id)
